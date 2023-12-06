ecf1baab265da2a6b283e8a8c0613dcff87bdc5c
IEatSushi <2klaus.4peter@gmail.com>
2023-12-06 07:53:18 +0100

    day 6

--- 2023/day06.py
+++ 2023/day06.py
@@ -3,9 +3,28 @@
 import re
 import numpy as np
 
-with open("day06_input copy.txt") as file:
-# with open("day06_input.txt") as file:
+# with open("day06_input copy.txt") as file:
+with open("day06_input.txt") as file:
 	text = file.read().splitlines()
 
-for i, line in enumerate(text):
-	
+times = [int(s) for s in re.findall("\d+", text[0][9:])]
+max_dists = [int(s) for s in re.findall("\d+", text[1][9:])]
+improves = []
+
+for i in range(len(times)):
+	time = times[i]
+	max_dist = max_dists[i]
+	improves.append(0)
+
+	for j in range(time):
+		dist = (time - j) * j
+		if dist > max_dist:
+			improves[i] += 1
+
+print(improves)
+sums = 1
+
+for i in improves:
+	sums *= i
+
+print(sums)
---
+++ 2023/day06b.py
@@ -0,0 +1,52 @@
+# Day 6
+
+import re
+import numpy as np
+
+def get_dist(race_time, acc_time):
+	return (race_time - acc_time) * acc_time
+
+# with open("day06_input copy.txt") as file:
+with open("day06_input.txt") as file:
+	text = file.read().splitlines()
+
+time = int(text[0][9:].replace(" ", ""))
+max_dist = int(text[1][9:].replace(" ", ""))
+
+print(time, max_dist)
+min_improve = -1
+max_improve = -1
+
+min = 0
+max = time
+
+while True:
+	mid = (min + max) // 2
+	
+	if get_dist(time, mid) <= max_dist and get_dist(time, mid + 1) > max_dist:
+		min_improve = mid + 1
+		break
+	
+	if get_dist(time, mid) <= max_dist:
+		min = mid
+	else:
+		max = mid
+
+
+min = 0
+max = time
+
+while True:
+	mid = (min + max) // 2
+	
+	if get_dist(time, mid) <= max_dist and get_dist(time, mid - 1) > max_dist:
+		max_improve = mid - 1
+		break
+	
+	if get_dist(time, mid) <= max_dist:
+		max = mid
+	else:
+		min = mid
+print(min_improve, max_improve)
+print(max_improve - min_improve + 1)
+
