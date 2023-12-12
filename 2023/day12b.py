aad5ac068e5aeeeac9073484e26b0e445beb4711
IEatSushi <2klaus.4peter@gmail.com>
2023-12-12 20:43:58 +0100

    day 12 - I should have known sooner

--- 2023/day12b.py
+++ 2023/day12b.py
@@ -21,23 +21,28 @@ def get_left_row(row, start):
 def is_not_hash(row, i):
 	return i < 0 or i >= len(row) or row[i] != "#"
 
-def get_pattern_num(row, pattern, indent=0):
-	min_chars = sum(pattern) + len(pattern) - 1
+solitions = {}
 
-	if (len(row) < min_chars):
+def get_pattern_num(row, pattern, indent=0):
+	problem = (row, tuple(pattern))
+	solition = solitions.get(problem, -1)
+
+	if solition != -1:
+		return solition
+
+	good_chars = len(row) - row.count(".")
+	if good_chars < sum(pattern):
 		return 0
 	next = pattern[0]
 	sums = 0
+	
+	min_chars = sum(pattern) + len(pattern) - 1
 	last_width = len(row) - min_chars + 1
 
 	for i in range(0, last_width):
 		if is_not_hash(row, i-1) and is_not_hash(row, i+next) and "." not in row[i:i+next]:
-			# space(indent)
-			# print(row[0:i] + "■"*next + row[i+next:])
 			if len(pattern) == 1:
 				if "#" not in row[i+next:]:
-					# space(indent)
-					# print("X")
 					sums += 1
 			else:
 				new_start = get_left_row(row, i + next)
@@ -45,6 +50,7 @@ def get_pattern_num(row, pattern, indent=0):
 					sums += get_pattern_num(row[new_start:], pattern[1:], indent + new_start)
 		if row[i] == "#":
 			break
+	solitions[problem] = sums
 	return sums
 
 
@@ -53,19 +59,16 @@ patterns = []
 
 for i, line in enumerate(text):
 	parts = line.split()
-	rows.append(parts[0])
-	patterns.append([int(x) for x in parts[1].split(",")])
-
-# idx = 2
-# print(rows[idx], patterns[idx])
-# print(get_pattern_num(rows[idx], patterns[idx]))
+	# springs.append([x for x in parts[0]])
+	rows.append("?".join([parts[0] for _ in range(5)]))
+	patterns.append([int(x) for x in parts[1].split(",")] * 5)
 
 sums = 0
-for i, pattern in enumerate(patterns):
+from tqdm import tqdm
+
+for i in range(len(rows)):
+	pattern = patterns[i]
 	row = rows[i]
 	col_idx = 0
-	# print(get_pattern_num(row, pattern))
 	sums += get_pattern_num(row, pattern)
 print(sums)
-	
-		
