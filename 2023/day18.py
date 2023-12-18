e9770aa75a5c04050bf650ca0455e4a749871e38
IEatSushi <2klaus.4peter@gmail.com>
2023-12-18 20:06:06 +0100

    WIP day 18 part 2

--- 2023/day18.py
+++ 2023/day18.py
@@ -3,9 +3,47 @@
 import re
 import numpy as np
 
-with open("day18_input copy.txt") as file:
-# with open("day18_input.txt") as file:
+# with open("day18_input copy.txt") as file:
+with open("day18_input.txt") as file:
 	text = file.read().splitlines()
 
+size = 10000
+field = np.zeros((2*size, 2*size))
+current = np.array([size, size])
+
+dirs = {
+	"L": np.array([-1, 0]),
+	"R": np.array([1, 0]),
+	"D": np.array([0, 1]),
+	"U": np.array([0, -1])
+}
+
+holes = 0
+
 for i, line in enumerate(text):
-	
+	parts = line.split()
+	dir = dirs[parts[0]]
+	times = int(parts[1])
+
+	for i in range(times):
+		next = current + dir
+		if (field[next[0], next[1]] != 1):
+			field[next[0], next[1]] = 1
+			holes += 1
+		current = next
+
+print("trench", holes)
+ends = [np.array([size + 1, size + 1])]
+
+while (len(ends) > 0):
+	current = ends.pop()
+
+	for dir in dirs.values():
+		next = current + dir
+		if field[next[0], next[1]] != 1:
+			field[next[0], next[1]] = 1
+			ends.append(next)
+			holes += 1
+
+
+print("lagoon", holes)
---
+++ 2023/day18b.py
@@ -0,0 +1,107 @@
+# Day 18
+
+import re
+import numpy as np
+
+with open("day18_input copy.txt") as file:
+# with open("day18_input.txt") as file:
+	text = file.read().splitlines()
+
+class YLine:
+	def __init__(self, x, y1, y2) -> None:
+		self.x = x
+		self.y_min = min(y1, y2)
+		self.y_max = max(y1, y2)
+
+	def intersects(self, y):
+		return self.y_min <= y and self.y_max >= y
+
+	# def intersect_type(self, y):
+		# if 
+
+dirs = {
+	"0": np.array([1, 0]),
+	"1": np.array([0, 1]),
+	"2": np.array([-1, 0]),
+	"3": np.array([0, -1])
+}
+
+current = np.array([0, 0])
+lagoon = []
+
+for i, line in enumerate(text):
+	parts = line.split()[2][2:-1]
+
+	dir = dirs[parts[5]]
+	times = int(parts[0:5], 16)
+	next = current + dir * times
+
+	if dir[0] == 0:
+		lagoon.append(YLine(current[0], current[1], next[1]))
+	
+	print(current, next)
+	current = next
+	
+
+print("trench", len(lagoon))
+ends = [np.array([1, 1])]
+
+lagoon.sort(key=lambda x: x.x)
+
+y_start = min(lagoon, key=lambda x: x.y_min).y_min
+y_end = max(lagoon, key=lambda x: x.y_max).y_max
+
+bay_area = 0
+
+y_steps = set(line.y_min for line in lagoon)
+y_steps.add(y_end + 1)
+y_steps = sorted(list(y_steps))
+				 
+for i, y in enumerate(y_steps[:-1]):
+	print("---", y, "---")
+
+	y_next = y_steps[i + 1]
+	height = y_next - y
+
+	inside = False
+	hit_min = False
+	hit_max = False
+	last_intersect = None
+
+	for line in lagoon:
+		if not line.intersects(y):
+			continue
+		new_intersect = line.x
+
+		if y == line.y_min:
+			if hit_max:
+				inside = not inside
+				hit_min = False
+				hit_max = False
+				bay_area += (new_intersect - last_intersect)
+			else:
+				hit_min = True
+				hit_max = False
+		elif y == line.y_max:
+			if hit_min:
+				inside = not inside
+				hit_min = False
+				hit_max = False
+				bay_area += (new_intersect - last_intersect)
+			else:
+				hit_min = False
+				hit_max = True
+		else:
+			inside = not inside
+			hit_min = False
+			hit_max = False
+		print(line.x, "in" if inside else "out")
+
+		if not inside:
+			print("increase ", (new_intersect - last_intersect + 1), "*", height)
+			bay_area += (new_intersect - last_intersect + 1) * height
+		last_intersect = new_intersect
+
+print()
+print(bay_area)
+
