c1ea9a2864d85de68201de6b4f31c83f26180724
Aaron <2klaus.4peter@gmail.com>
2023-12-27 23:18:51 +0100

    finish day 14 part 2
    i swear i need to re-learn reading, and where east and west is

--- 2023/day14.py
+++ 2023/day14.py
@@ -3,8 +3,8 @@
 import re
 import numpy as np
 
-with open("day14_input copy.txt") as file:
-# with open("day14_input.txt") as file:
+# with open("day14_input copy.txt") as file:
+with open("day14_input.txt") as file:
 	text = file.read().splitlines()
 
 w = len(text[0])
@@ -19,31 +19,72 @@ for y, line in enumerate(text):
 			field[x,y] = 2
 
 def roll_x(field, x, y, dx):
-	maxx = 0 if dx < 0 else x
-	minx = field.shape[0] if dx < 0 else x
+	max_x = field.shape[0] - 1 if dx > 0 else 0
 
-	for i in range(minx, maxx, dx):
+	for i in range(x, max_x, dx):
 		if field[i + dx, y] != 0:
 			break
 		field[i, y] = 0
 		field[i + dx, y] = 2
 
-def roll_y(field, x, y, dy):
-	maxy = 0 if dy < 0 else y
-	miny = field.shape[1] if dy < 0 else y
 
-	for i in range(miny, maxy, dy):
-		# print("check", i-1, field[x,i-1])
-		if field[x,i-1] != 0:
+def roll_y(field, x, y, dy):
+	max_y = field.shape[1] - 1 if dy > 0 else 0
+
+	for i in range(y, max_y, dy):
+		if field[x, i + dy] != 0:
 			break
 		field[x, i] = 0
-		field[x, i - 1] = 2
+		field[x, i + dy] = 2
+
+signs = [".", "#", "O"]
+
+def display():
+	for y in range(h):
+		for x in range(w):
+			print(signs[field[x,y]], end="")
+		print("")
+	print("")
 
 
-for y in range(h):
+def cycle(left):
+	for y in range(h):
+		for x in range(w):
+			if field[x, y] == 2:
+				roll_y(field, x, y, -1)
+
 	for x in range(w):
-		if field[x,y] == 2:
-			roll_y(field, x, y, -1)
+		for y in range(h):
+			if field[x, y] == 2:
+				roll_x(field, x, y, -1)
+
+	for y in range(h-1, -1, -1):
+		for x in range(w):
+			if field[x, y] == 2:
+				roll_y(field, x, y, 1)
+
+	for x in range(w-1, -1, -1):
+		for y in range(h):
+			if field[x, y] == 2:
+				roll_x(field, x, y, 1)
+
+
+states = []
+check_repeat = True
+times = 1000000000
+
+while(times > 0):
+	cycle(times)
+	times -= 1
+	states.insert(0, field.copy())
+
+	if check_repeat:
+		for i in range(1, len(states)):
+			if np.array_equal(field, states[i]):
+				print(times, "repeat after", i)
+				times %= i
+				print("cycle", times, "more")
+				check_repeat = False
 
 sums = 0
 
@@ -54,8 +95,4 @@ for y in range(h):
 
 print(sums)
 
-signs = [".", "#", "O"]
-for y in range(h):
-	for x in range(w):
-		print(signs[field[x,y]], end="")
-	print()
+
