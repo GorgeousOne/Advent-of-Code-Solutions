73818aee1e81cfc1ee2216386689ec2c777b6d65
Aaron <2klaus.4peter@gmail.com>
2024-01-02 00:31:53 +0100

    WIP day 23 day 2

--- 2023/day21b.py
+++ 2023/day21b.py
@@ -3,6 +3,11 @@
 import re
 import numpy as np
 
+with open("day21_input.txt") as file:
+	text = file.read()
+	some = (text.count(".") + 1) / (131 * 131)
+	print(some)
+
 # with open("day21_input copy.txt") as file:
 with open("day21_input.txt") as file:
 	text = file.read().splitlines()
@@ -18,43 +23,53 @@ for y, line in enumerate(text):
 		elif letter == "S":
 			start = (x, y)
 
-dirs = [
-	np.array([-1, 0]),
-	np.array([1, 0]),
-	np.array([0, 1]),
-	np.array([0, -1])
-]
 
-def wrap_pos(pos, max_x, max_y):
-	return (pos[0] % max_x, pos[1] % max_y)
+def count_free(area_fun):
+	evens = 0
+	odds = 0
 
-final_reach = set()
+	for y in range(h):
+		for x in range(w):
+			if not area_fun(x, y):
+				continue
+			if garden[x, y] == 1:
+				continue
+			if (x + y) % 2 == 0:
+				evens += 1
+			else:
+				odds += 1
+	return evens, odds
 
-print(w, h)
 
-steps = 26501365
 
-square_reach = (steps - 65) // w
+free_mid = count_free(lambda x, y: abs(x - 65) + abs(y - 65) <= 65)
+free_tl = count_free(lambda x, y: x + y < 65)
+free_bl = count_free(lambda x, y: x + (130 - y) < 65)
+free_tr = count_free(lambda x, y: (130 - x) + y < 65)
+free_br = count_free(lambda x, y: (130 - x) + (130 - y) < 65)
 
-n = square_reach - 1
-fulls = (n * (n + 1) * (2 * n + 1)) // 6
-# print(fulls)
+combined = [free_mid, free_bl, free_br, free_tl, free_tr]
+steps = 26_501_365
 
-all_count = np.count_nonzero(garden)
+radius = (steps) // w + 1
 
-counts1 = 0
-counts2 = 0
-counts3 = 0
-counts4 = 0
-for x in range(65):
-	for y in range(65 - x):
-		if garden[x, y] == 1:
-			counts1 += 1
-		if garden[w - 1 - x, y] == 1:
-			counts2 += 1
-		if garden[w - 1 - x, h - 1 - y] == 1:
-			counts3 += 1
-		if garden[x, h - 1 - y] == 1:
-			counts4 += 1
+even_squares = radius * radius
+odd_squares = (radius - 1) * (radius - 1)
 
-print(counts1, counts2, counts3, counts4)
+even_corners = even_squares + (radius - 1)
+odd_corners = odd_squares - radius
+
+free_plots = even_squares * free_mid[0] + odd_squares * free_mid[1]
+free_plots += even_corners * free_bl[0] + odd_corners * free_bl[1]
+free_plots += even_corners * free_tl[0] + odd_corners * free_tl[1]
+free_plots += even_corners * free_br[0] + odd_corners * free_br[1]
+free_plots += even_corners * free_tl[0] + odd_corners * free_tr[1]
+
+
+free = 0
+
+for x in combined:
+	free += x[0] + x[1]
+
+print((steps * steps + (steps-1) * (steps-1)) * some)
+print(free_plots)
