f5c6c6339608ee1fc21b32150eab9172e4d7222a
Aaron <2klaus.4peter@gmail.com>
2024-12-04 06:45:58 +0100

    day 04

---
+++ 2024/04.py
@@ -0,0 +1,41 @@
+
+# with open("04test.txt", "r") as f:
+with open("04input.txt", "r") as f:
+	t = f.read().splitlines()
+
+dirs = [
+	(-1, -1),
+	(-1, 0),
+	(-1, 1),
+	(0, -1),
+	(0, 0),
+	(0, 1),
+	(1, -1),
+	(1, 0),
+	(1, 1)
+]
+
+num = 0
+mas = "MAS"
+
+for y, l in enumerate(t):
+	for x, c in enumerate(l):
+		if c != "X":
+			continue
+		for d in dirs:
+			x2 = x
+			y2 = y
+			isXmas = True
+			for i in range(3):
+				x2 += d[0]
+				y2 += d[1]
+				if y2 < 0 or y2 >= len(t) or x2 < 0 or x2 >= len(l):
+					isXmas = False
+					break
+				if t[y2][x2] != mas[i]:
+					isXmas = False
+					break
+			if isXmas:
+				num += 1
+
+print(num)
---
+++ 2024/04b.py
@@ -0,0 +1,19 @@
+
+# with open("04test.txt", "r") as f:
+with open("04input.txt", "r") as f:
+	t = f.read().splitlines()
+
+num = 0
+
+for y, l in enumerate(t):
+	for x, c in enumerate(l):
+		if c != "A":
+			continue
+		if x < 1 or x >= len(l)-1 or y < 1 or y >= len(t)-1:
+			continue
+		d1 = [t[y-1][x-1], t[y+1][x+1]]
+		d2 = [t[y-1][x+1], t[y+1][x-1]]
+
+		if "M" in d1 and "S" in d1 and "M" in d2 and "S" in d2:
+			num += 1
+print(num)
