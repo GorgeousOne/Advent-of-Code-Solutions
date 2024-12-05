ae3d39e280a52d398faddf9cb945fc2bd504664c
Aaron <2klaus.4peter@gmail.com>
2024-12-05 10:20:48 +0100

    day 05

---
+++ 2024/05.py
@@ -0,0 +1,36 @@
+"""
+find all rows (lower input) where no y is listed before an x
+given by the x|y rules (upper input)
+"""
+
+# with open("05test.txt", "r") as f:
+with open("05input.txt", "r") as f:
+	t = f.read().splitlines()
+
+rs = {}
+ups = []
+
+for l in t:
+	#rule
+	if '|' in l:
+		r1, r2 = l.split('|')
+		if r1 not in rs:
+			rs[r1] = []
+		rs[r1].append(r2)
+	#row
+	elif ',' in l:
+		ups.append(l.split(','))
+
+# print("\n".join([str(x) for x in rs.items()]))
+c = 0
+
+for j, u in enumerate(ups):
+	is_correct = True
+	for i, x in enumerate(u):
+
+		if x in rs and any([y in u[:i] for y in rs[x]]):
+			is_correct = False
+			break
+	if is_correct:
+		c += int(u[len(u) // 2])
+print(c)
---
+++ 2024/05b.py
@@ -0,0 +1,44 @@
+"""
+sort all rows (lower input) where an y is listed before an x
+given by the x|y rules (upper input) so that each row
+satisfies all rules
+"""
+from collections import defaultdict
+
+# with open("05test.txt", "r") as f:
+with open("05input.txt", "r") as f:
+	t = f.read().splitlines()
+
+rs = defaultdict(list)
+ups = []
+for l in t:
+	if '|' in l:
+		r1, r2 = l.split('|')
+		rs[r1].append(r2)
+	elif ',' in l:
+		ups.append(l.split(','))
+
+# print("\n".join([str(x) for x in rs.items()]))
+
+def is_c(u, rs):
+	for i, x in enumerate(u):
+		if x in rs and any([y in u[:i] for y in rs[x]]):
+			return False
+	return True
+
+c = 0
+cs = []
+for j, u in enumerate(ups):
+	if is_c(u, rs):
+		continue
+	while not is_c(u, rs):
+		for k, x in enumerate(u):
+			if x not in rs:
+				continue
+			for y in rs[x]:
+				if y in u[:k]:
+					u.remove(x)
+					u.insert(u.index(y), x)
+					break
+	c += int(u[len(u) // 2])
+print(c)
